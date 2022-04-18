9b8cf29ee85c6713930774c135e06b7df923fb09
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-04-18 16:08:51 -0500

    Se da funcinalidad al registro de usuarios nuevosy comprobación de terminos y condiciones

--- app/src/main/java/com/richarwhiteg2/wildrunning/LoginActivity.kt
+++ app/src/main/java/com/richarwhiteg2/wildrunning/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.richarwhiteg2.wildrunning
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
@@ -7,6 +8,11 @@ import android.widget.LinearLayout
 import com.google.firebase.auth.FirebaseAuth
 import kotlin.properties.Delegates
 import android.view.View
+import android.widget.CheckBox
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
+import java.text.SimpleDateFormat
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -34,4 +40,56 @@ class LoginActivity : AppCompatActivity() {
         etPassword = findViewById(R.id.etPassword)
         mAuth = FirebaseAuth.getInstance()
     }
+    fun login(view: View) {
+        loginUser()
+    }
+    private fun loginUser(){
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+
+        mAuth.signInWithEmailAndPassword(email, password) //para entrar con email y password
+            .addOnCompleteListener(this){ task ->  //task nos indica si el incio fue satisfactorio
+                if (task.isSuccessful)  goHome(email, "email")  //loggin exitoso manda a pantalla principal
+                else{
+                    if (lyTerms.visibility == View.INVISIBLE) lyTerms.visibility = View.VISIBLE
+                    else{
+                        var cbAcept = findViewById<CheckBox>(R.id.cbAcept)
+                        if (cbAcept.isChecked) register()
+                    }
+                }
+            }
+
+    }
+    //Si el loggin fue exitoso esta funcion envia a la panatalla principal de la app
+    private fun goHome(email: String, provider: String){
+
+        useremail = email
+        providerSession = provider
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun register(){
+        email = etEmail.text.toString()
+        password = etPassword.text.toString()
+
+        //crea el usuario en Firebase
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {
+                if (it.isSuccessful){
+
+                    var dateRegister = SimpleDateFormat("dd/MM/yyyy").format(Date()) //Para guardar la fecha
+                    var dbRegister = FirebaseFirestore.getInstance() //Para acceder a la base da dates y acceder a la colecciones
+                    dbRegister.collection("users").document(email).set(hashMapOf(
+                        "user" to email,
+                        "dateRegister" to dateRegister
+                    ))
+
+                    goHome(email, "email")
+                }
+                else Toast.makeText(this, "Error, algo ha salido mal", Toast.LENGTH_SHORT).show()
+            }
+    }
+
 }
