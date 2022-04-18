b6ed3a3183be74d0e414e99e23b3e7943f9989b1
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-04-18 15:43:54 -0500

    se agregan Librerias y variables para inicio de sesión. asi como configuracion del gradle app

--- app/src/main/java/com/richarwhiteg2/wildrunning/LoginActivity.kt
+++ app/src/main/java/com/richarwhiteg2/wildrunning/LoginActivity.kt
@@ -2,10 +2,36 @@ package com.richarwhiteg2.wildrunning
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.LinearLayout
+import com.google.firebase.auth.FirebaseAuth
+import kotlin.properties.Delegates
+import android.view.View
 
 class LoginActivity : AppCompatActivity() {
+
+    companion object{   //PAra que se tenga la referencia en todas las clases   companion object
+        lateinit var useremail: String
+        lateinit var providerSession: String
+    }
+
+    private var email by Delegates.notNull<String>() // Delegates No permite que sea nulo
+    private var password by Delegates.notNull<String>()
+    private lateinit var etEmail: EditText
+    private lateinit var etPassword: EditText
+    private lateinit var lyTerms: LinearLayout  //terminos y condiciones
+
+    private lateinit var mAuth: FirebaseAuth //autenticación de base de datos FIREBASE
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        //Para que se oculten los terminos y condiciones
+        lyTerms = findViewById(R.id.lyTerms)
+        lyTerms.visibility = View.INVISIBLE
+
+        etEmail = findViewById(R.id.etEmail)
+        etPassword = findViewById(R.id.etPassword)
+        mAuth = FirebaseAuth.getInstance()
     }
 }
