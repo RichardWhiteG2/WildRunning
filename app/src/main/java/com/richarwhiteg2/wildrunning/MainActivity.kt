f180e6b024636f6b9d7227e352a75f1113adeb28
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-05-10 23:34:02 -0500

    se comienza amodificar el MainActivity.kt

--- app/src/main/java/com/richarwhiteg2/wildrunning/MainActivity.kt
+++ app/src/main/java/com/richarwhiteg2/wildrunning/MainActivity.kt
@@ -5,14 +5,32 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.richarwhiteg2.wildrunning.LoginActivity.Companion.useremail
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawer: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "Hola $useremail", Toast.LENGTH_SHORT).show()
+        //Toast.makeText(this, "Hola $useremail", Toast.LENGTH_SHORT).show() saluda al email que ingresa
+        initToolBar()
+
+    }
+    private fun initToolBar(){
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+
+        drawer = findViewById(R.id.drawer_layout)
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.bar_title,
+            R.string.navigation_drawer_close)
+
+        drawer.addDrawerListener (toggle)
+        toggle.syncState()
     }
     fun callSignOut(view: View){
         signOut()
