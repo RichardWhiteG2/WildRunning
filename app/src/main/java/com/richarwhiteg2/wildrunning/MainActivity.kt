5397a9ea174980b06dd2c46ee1a8c7e109145dd6
Brayan Ricardo Blanco García <102545085+RichardWhiteG2@users.noreply.github.com>
2022-05-17 16:12:35 -0500

    realizacion de animaciones, se queda en clase traslation z 307

--- app/src/main/java/com/richarwhiteg2/wildrunning/MainActivity.kt
+++ app/src/main/java/com/richarwhiteg2/wildrunning/MainActivity.kt
@@ -7,23 +7,45 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.richarwhiteg2.wildrunning.LoginActivity.Companion.useremail
+import com.richarwhiteg2.wildrunning.Utility.animateViewofFloat
+import com.richarwhiteg2.wildrunning.Utility.animateViewofInt
+import com.richarwhiteg2.wildrunning.Utility.setHeightLinearLayout
+import me.tankery.lib.circularseekbar.CircularSeekBar
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var drawer: DrawerLayout
+
+    private lateinit var swIntervalMode: Switch
+    private lateinit var swChallenges: Switch
+    private lateinit var swVolumes: Switch
+
+    private lateinit var npChallengeDistance: NumberPicker
+    private lateinit var npChallengeDurationHH: NumberPicker
+    private lateinit var npChallengeDurationMM: NumberPicker
+    private lateinit var npChallengeDurationSS: NumberPicker
+
+    private var challengeDistance: Float = 0f
+    private var challengeDuration: Int = 0
+
+    private lateinit var tvChrono: TextView
+
+
+    private lateinit var csbRunWalk: CircularSeekBar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //Toast.makeText(this, "Hola $useremail", Toast.LENGTH_SHORT).show() saluda al email que ingresa
         initToolBar()
+        initObjects()
         initNavigationView()
     }
     override fun onBackPressed() {
@@ -58,6 +80,72 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         var tvUser: TextView = headerView.findViewById(R.id.tvUser)
         tvUser.text= useremail
     }
+    private fun initObjects(){
+        /*tvChrono = findViewById(R.id.tvChrono)
+        tvChrono.setTextColor(ContextCompat.getColor( this, R.color.white))
+        initStopWatch()*/
+
+        var lyMap = findViewById<LinearLayout>(R.id.lyMap)
+        var lyFragmentMap = findViewById<LinearLayout>(R.id.lyFragmentMap)
+        val lyIntervalModeSpace = findViewById<LinearLayout>(R.id.lyIntervalModeSpace)
+        val lyIntervalMode = findViewById<LinearLayout>(R.id.lyIntervalMode)
+        val lyChallengesSpace = findViewById<LinearLayout>(R.id.lyChallengesSpace)
+        val lyChallenges = findViewById<LinearLayout>(R.id.lyChallenges)
+        val lySettingsVolumesSpace = findViewById<LinearLayout>(R.id.lySettingsVolumesSpace)
+        val lySettingsVolumes = findViewById<LinearLayout>(R.id.lySettingsVolumes)
+        var lySoftTrack = findViewById<LinearLayout>(R.id.lySoftTrack)
+        var lySoftVolume = findViewById<LinearLayout>(R.id.lySoftVolume)
+
+
+        setHeightLinearLayout(lyMap, 0)
+        setHeightLinearLayout(lyIntervalModeSpace,0)
+        setHeightLinearLayout(lyChallengesSpace,0)
+        setHeightLinearLayout(lySettingsVolumesSpace,0)
+        setHeightLinearLayout(lySoftTrack,0)
+        setHeightLinearLayout(lySoftVolume,0)
+
+        lyFragmentMap.translationY = -300f
+        lyIntervalMode.translationY = -300f
+        lyChallenges.translationY = -300f
+        lySettingsVolumes.translationY = -300f
+
+        swIntervalMode = findViewById(R.id.swIntervalMode)
+        swChallenges = findViewById(R.id.swChallenges)
+        swVolumes = findViewById(R.id.swVolumes)
+
+        csbRunWalk = findViewById(R.id.csbRunWalk)
+
+        npChallengeDistance = findViewById(R.id.npChallengeDistance)
+        npChallengeDurationHH = findViewById(R.id.npChallengeDurationHH)
+        npChallengeDurationMM = findViewById(R.id.npChallengeDurationMM)
+        npChallengeDurationSS = findViewById(R.id.npChallengeDurationSS)
+
+/*
+
+        csbRunWalk.setOnSeekBarChangeListener(object : OnCircularSeekBarChangeListener {
+            override fun onProgressChanged(circularSeekBar: CircularSeekBar,progress: Float,fromUser: Boolean) {
+
+                var STEPS_UX: Int = 15
+                var set: Int = 0
+                var p = progress.toInt()
+
+                if (p%STEPS_UX != 0){
+                    while (p >= 60) p -= 60
+                    while (p >= STEPS_UX) p -= STEPS_UX
+                    if (STEPS_UX-p > STEPS_UX/2) set = -1 * p
+                    else set = STEPS_UX-p
+
+                    csbRunWalk.progress = csbRunWalk.progress + set
+                }
+            }
+
+            override fun onStopTrackingTouch(seekBar: CircularSeekBar) {
+            }
+
+            override fun onStartTrackingTouch(seekBar: CircularSeekBar) {
+            }
+        })*/
+    }
     fun callSignOut(view: View){
         signOut()
     }
@@ -80,5 +168,42 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     private fun callRecordActivity(){
         val intent = Intent(this, RecordActivity::class.java)
         startActivity(intent)
+    }
+    fun inflateChallenges(v: View){
+        val lyChallengesSpace = findViewById<LinearLayout>(R.id.lyChallengesSpace)
+        val lyChallenges = findViewById<LinearLayout>(R.id.lyChallenges)
+        if (swChallenges.isChecked){
+            animateViewofInt(swChallenges, "textColor", ContextCompat.getColor(this, R.color.orange), 500)
+            setHeightLinearLayout(lyChallengesSpace, 750)
+            animateViewofFloat(lyChallenges, "translationY", 0f, 500)
+        }
+        else{
+            swChallenges.setTextColor(ContextCompat.getColor(this, R.color.white))
+            setHeightLinearLayout(lyChallengesSpace,0)
+            lyChallenges.translationY = -300f
+
+            challengeDistance = 0f
+            challengeDuration = 0
+        }
+    }
+    fun inflateVolumes(v: View){
+
+        val lySettingsVolumesSpace = findViewById<LinearLayout>(R.id.lySettingsVolumesSpace)
+        val lySettingsVolumes = findViewById<LinearLayout>(R.id.lySettingsVolumes)
+
+        if (swVolumes.isChecked){
+            animateViewofInt(swVolumes, "textColor", ContextCompat.getColor(this, R.color.orange), 500)
+            var swIntervalMode = findViewById<Switch>(R.id.swIntervalMode)
+            var value = 400
+            if (swIntervalMode.isChecked) value = 600
+
+            setHeightLinearLayout(lySettingsVolumesSpace, value)
+            animateViewofFloat(lySettingsVolumes, "translationY", 0f, 500)
+        }
+        else{
+            swVolumes.setTextColor(ContextCompat.getColor(this, R.color.white))
+            setHeightLinearLayout(lySettingsVolumesSpace,0)
+            lySettingsVolumes.translationY = -300f
+        }
     }
 }
